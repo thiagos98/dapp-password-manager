5b0f345f3a04601031104d1c31d724c858e891d9
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2022-04-26 17:45:36 -0400

    contrato corrigido

--- contracts/PasswordManager.sol
+++ contracts/PasswordManager.sol
@@ -7,7 +7,6 @@ contract PasswordManager {
     
     address[] listAddrPassword;
 
-    // create a function to save a password inside a mapping array in solidity
     function savePassword(address addr, string memory password) public {
         if (passwords[addr].length > 3) {
             passwords[addr].push(password);
@@ -15,27 +14,22 @@ contract PasswordManager {
         }
     }
     
-    // create a function to get a password inside a mapping array in solidity
     function getPassword(address addr, uint index) public view returns (string memory) {
         return passwords[addr][index];
     }
     
-    // create a function to get the number of passwords inside a mapping array in solidity
     function getNumPasswords(address addr) public view returns (uint) {
         return passwords[addr].length;
     }
     
-    // create a function to save an address inside a mapping array in solidity
     function saveAddress(address addr) internal {
         listAddrPassword.push(addr);
     }
     
-    // create a function to get an address inside a mapping array in solidity
     function getAddress(uint index) public view returns (address) {
         return listAddrPassword[index];
     }
     
-    // create a function to get the number of addresses inside a mapping array in solidity
     function getNumAddresses() public view returns (uint) {
         return listAddrPassword.length;
     }
